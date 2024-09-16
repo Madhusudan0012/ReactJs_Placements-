dd99899f10c285c936b64050e4835944b5366863
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-09-16 23:46:56 +0530

    toggle project

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -1,12 +1,13 @@
- 
+import Login from './Components/login'
+import Profile from './Components/profile'
 import UserContextProvider from './Context/USerContextProvider'
 
 function App() {
   return (
     <UserContextProvider>
       <h1> React with Chai and Share is Important </h1>
-      <login/>
-      <profile/>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
---
+++ 09themeswitcher/src/contexts/theme.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import {createContext , useContext} from 'react';
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme : () => {},
+    lightTheme : () => {},
+
+}) 
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
